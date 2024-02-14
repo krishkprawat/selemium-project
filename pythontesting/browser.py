ffe0f74551fc0d58d8f8118c3d7c6f1a5a125058
krishnapal singh rawat <krishkprawat@gmail.com>
2024-02-14 15:46:37 +0530

    new methods

--- pythontesting/browser.py
+++ pythontesting/browser.py
@@ -4,6 +4,11 @@ from selenium.webdriver.chrome.service import Service
 service_obj= Service()
 driver = webdriver.Chrome(service=service_obj)
 driver.get("https://rahulshettyacademy.com")
+
+driver.maximize_window() #to maximize the window
+driver.get("https://rahulshettyacademy.com/consulting")
+driver.minimize_window()
+driver.back() # to go back to previuos url
 print(driver.title)
 print(driver.current_url)
 driver.close()
