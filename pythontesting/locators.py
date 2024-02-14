23ece16bd798c8e7a2d56467136265cae4a54c34
krishnapal singh rawat <krishkprawat@gmail.com>
2024-02-14 16:59:52 +0530

    new method for new loators

--- pythontesting/locators.py
+++ pythontesting/locators.py
@@ -7,7 +7,7 @@ service_obj= Service()
 driver = webdriver.Chrome(service=service_obj)
 driver.get("https://rahulshettyacademy.com/angularpractice/")
 
-driver.find_element(By.NAME,"email").send_keys("shorya@gmail.com")
+driver.find_element(By.NAME,"email").send_keys("vijay@gmail.com")
 
 
 driver.find_element(By.ID,"exampleInputPassword1").send_keys("secretpassword")
@@ -16,5 +16,6 @@ dropdown_element=driver.find_element(By.ID,"exampleFormControlSelect1")
 dropdown=Select(dropdown_element)
 dropdown.select_by_visible_text("Male") # this is by providing the value
 #dropdown.select_by_index(0) # this is select  by index
+driver.find_element(By.CSS_SELECTOR,"input[value=option2]").click() #cssselector locator
 input("enter any key to close the browser") #this i added so tha browser cannot stop immediately
 
