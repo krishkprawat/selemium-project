a5d04245954bf7f02c4a3aae0efdaf0e5228e3ae
krishnapal singh rawat <krishkprawat@gmail.com>
2024-02-14 18:02:52 +0530

    new method for new loatorsxpath

---
+++ pythontesting/extract.py
@@ -0,0 +1,15 @@
+#extract the test from web page
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+service_obj= Service()
+driver = webdriver.Chrome(service=service_obj)
+driver.get("https://rahulshettyacademy.com/client/")
+
+
+
+input("enter any key to close the browser") #this i added so tha browser cannot stop immediately
+
