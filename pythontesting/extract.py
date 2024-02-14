87cf1f4406525c8060aeb57408a0185a273b63b5
krishnapal singh rawat <krishkprawat@gmail.com>
2024-02-14 19:50:18 +0530

    new method for login page and forgot password

--- pythontesting/extract.py
+++ pythontesting/extract.py
@@ -9,6 +9,13 @@ service_obj= Service()
 driver = webdriver.Chrome(service=service_obj)
 driver.get("https://rahulshettyacademy.com/client/")
 
+driver.find_element(By.LINK_TEXT,"Forgot password?").click()
+
+driver.find_element(By.XPATH,"//input[@formcontrolname='userEmail']").send_keys('krishnapal@gmail.com')
+driver.find_element(By.ID, "userPassword").send_keys("boohoo123#")
+driver.find_element(By.ID, "confirmPassword").send_keys("boohoo123#")
+driver.find_element(By.XPATH,"//button[text()='Save New Password']").click()
+
 
 
 input("enter any key to close the browser") #this i added so tha browser cannot stop immediately
