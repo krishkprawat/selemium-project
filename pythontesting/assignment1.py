56c67e34820425a109f29172e253f9e078247796
krishnapal singh rawat <krishkprawat@gmail.com>
2024-02-15 17:48:14 +0530

    swindow seitchedd

--- pythontesting/assignment1.py
+++ pythontesting/assignment1.py
@@ -5,14 +5,21 @@ from selenium.webdriver.support.select import Select
 
 service_obj = Service()
 driver = webdriver.Chrome(service=service_obj)
+
+driver.implicitly_wait(10)
+
 driver.get("https://rahulshettyacademy.com/loginpagePractise/")
 driver.find_element(By.LINK_TEXT,"Free Access to InterviewQues/ResumeAssistance/Material").click()
 windowsOpened = driver.window_handles
 driver.switch_to.window(windowsOpened[1])
 
 mygrab=driver.find_element(By.CSS_SELECTOR,".red").text
-emails = [word for word in mygrab.split() if '@' in word and '.' in word]
+emails = ' '.join(word for word in mygrab.split() if '@' in word and '.' in word)
 print(emails)
+
+#switch to main tab again
+driver.switch_to.window(windowsOpened[0])
+driver.find_element(By.ID,"username").send_keys(emails)
 
 
 
