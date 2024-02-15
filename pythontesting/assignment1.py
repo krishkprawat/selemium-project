b57939d9b762ecdb577532d3d432201f6a43abaa
krishnapal singh rawat <krishkprawat@gmail.com>
2024-02-15 18:35:39 +0530

    swindow seitchedd

--- pythontesting/assignment1.py
+++ pythontesting/assignment1.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
 
 service_obj = Service()
 driver = webdriver.Chrome(service=service_obj)
@@ -20,8 +22,13 @@ print(emails)
 #switch to main tab again
 driver.switch_to.window(windowsOpened[0])
 driver.find_element(By.ID,"username").send_keys(emails)
+driver.find_element(By.ID,"password").send_keys("hahahaha")
+driver.find_element(By.PARTIAL_LINK_TEXT,"terms and conditions").click()
+driver.find_element(By.ID, "signInBtn").click()
 
+wait= WebDriverWait(driver,10)
 
-
-
+wait.until(expected_conditions.visibility_of_element_located((By.CSS_SELECTOR,".alert-danger")))
+alertmessage=driver.find_element(By.CSS_SELECTOR,".alert-danger").text
+#print(alertmessage)
 input("insert any  key to stop")
