609e2691ed868c6b9e9d910a51daafcac75fbe43
krishnapal singh rawat <krishkprawat@gmail.com>
2024-02-15 19:04:55 +0530

    alert print

---
+++ pythontesting/cart.py
@@ -0,0 +1,21 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+
+service_obj = Service()
+driver = webdriver.Chrome(service=service_obj)
+
+driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
+driver.find_element(By.CSS_SELECTOR,"input[class=search-keyword]").send_keys("roo")
+
+time.sleep(4)
+
+
+
+
+input("insert any  key to stop")
