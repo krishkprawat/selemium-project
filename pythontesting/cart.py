dc1ed8761fb856f158b2713075f727c54e68dd95
krishnapal singh rawat <krishkprawat@gmail.com>
2024-02-16 14:06:16 +0530

    cart process

--- pythontesting/cart.py
+++ pythontesting/cart.py
@@ -18,12 +18,32 @@ for result in results:
 driver.find_element(By.XPATH, "//img[@alt='Cart']").click()
 driver.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
 # starts a new page
-
+prices = driver.find_elements(By.XPATH, "//tr/td[5]/p")
+sum = 0
+for price in prices:
+    sum = sum + int(price.text)
+print(sum)
+total = int(driver.find_element(By.CSS_SELECTOR, ".totAmt").text)
+assert total == sum
 driver.find_element(By.XPATH, "//input[@class='promoCode']").send_keys("rahulshettyacademy")
-#driver.find_element(By.CSS_SELECTOR, "input[class='promoCode]").send_keys("rahulshettyacademy")
+# driver.find_element(By.CSS_SELECTOR, "input[class='promoCode]").send_keys("rahulshettyacademy")
 driver.find_element(By.XPATH, "//button[@class='promoBtn']").click()
 wait = WebDriverWait(driver, 10)
 wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, "span[class='promoInfo']")))
-print(driver.find_element(By.CSS_SELECTOR, ".promoInfo").text)
+alert = driver.find_element(By.CSS_SELECTOR, ".promoInfo").text
+assert "Code applied" in alert
+
+wait = WebDriverWait(driver, 10)
+wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, "span[class='promoInfo]")))
+discount=driver.find_element(By.CSS_SELECTOR, "span[class='discountPerc']").text
+discount_amount=driver.find_element(By.CSS_SELECTOR,".discountAmt")
+def calculate(total,discount):
+    discount_value= (total*discount)/100
+    new=total-discount_value
+    return new
+
+
+assert new == discount_amount
 
 input("insert any  key to stop")
+
